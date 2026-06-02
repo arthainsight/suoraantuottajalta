8f86097b0cc3c31eb0327af0ef3438d6a0199eee
arthainsight <riku.forsell@gmail.com>
2026-06-02 10:38:43 +0300

    Add site footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+export function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container footer-inner">
+        <strong>Suoraan Tuottajalta</strong>
+        <span>
+          Kasvava hakemisto suomalaisista lähiruoan suoramyynti- ja lyhyen
+          ketjun ostopaikoista.
+        </span>
+      </div>
+    </footer>
+  );
+}
