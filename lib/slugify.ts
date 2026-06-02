d4cc49ff95c4f07a226d2920ebe60733a6e93528
arthainsight <riku.forsell@gmail.com>
2026-06-02 10:36:09 +0300

    Add slugify helper

---
+++ lib/slugify.ts
@@ -0,0 +1,11 @@
+export function slugify(value: string): string {
+  return value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/ä/g, "a")
+    .replace(/ö/g, "o")
+    .replace(/å/g, "a")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
