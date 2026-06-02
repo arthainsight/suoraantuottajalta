70a1697489a0c850fedefc80332936719ca42301
arthainsight <riku.forsell@gmail.com>
2026-06-02 11:04:40 +0300

    Make place filters clickable

--- components/PlaceFilters.tsx
+++ components/PlaceFilters.tsx
@@ -1,21 +1,39 @@
-const filters = [
-  "Tilamyymälät",
-  "REKO-renkaat",
-  "Torit",
-  "Lähiruokapuodit",
-  "Verkkokaupat",
-  "Satokassit",
-  "Varmennettu tieto",
+import Link from "next/link";
+
+const typeFilters = [
+  { label: "REKO-renkaat", href: "/hakemisto?type=reko" },
+  { label: "Torit", href: "/hakemisto?type=tori" },
+  { label: "Tilamyymälät", href: "/hakemisto?type=tilamyymala" },
+  { label: "Lähiruokapuodit", href: "/hakemisto?type=lahiruokapuoti" },
+  { label: "Verkkokaupat", href: "/hakemisto?type=verkkokauppa" },
+];
+
+const regionFilters = [
+  { label: "Uusimaa", href: "/hakemisto?region=Uusimaa" },
+  { label: "Varsinais-Suomi", href: "/hakemisto?region=Varsinais-Suomi" },
+  { label: "Etelä-Savo", href: "/hakemisto?region=Etelä-Savo" },
 ];
 
 export function PlaceFilters() {
   return (
-    <div className="filters" aria-label="Kohdesuodattimet">
-      {filters.map((filter) => (
-        <button className="filter" type="button" key={filter}>
-          {filter}
-        </button>
-      ))}
+    <div>
+      <div className="filters" aria-label="Myyntitavan suodattimet">
+        <Link className="filter" href="/hakemisto">
+          Kaikki
+        </Link>
+        {typeFilters.map((filter) => (
+          <Link className="filter" href={filter.href} key={filter.label}>
+            {filter.label}
+          </Link>
+        ))}
+      </div>
+      <div className="filters" aria-label="Maakunnan suodattimet">
+        {regionFilters.map((filter) => (
+          <Link className="filter" href={filter.href} key={filter.label}>
+            {filter.label}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
